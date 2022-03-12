92638ebe0011ed860990c5020bb5ea1ded09596b
Kalashkov Paul <kalashkovpaul@yandex.ru>
2022-03-12 11:24:05 +0300

    Lab02 is done

--- lab_01/main.py
+++ lab_01/main.py
@@ -1,163 +1,167 @@
-import numpy as np
-from table import *
+from cmath import inf
+import copy
 
-def divided_difference(xs, ys, n):
-    def _dd(yi, yj, xi, xj):
-        denom = 1e-12 if xi == xj else xi - xj
-        return (yi - yj) / denom
-    dd = np.zeros((n, n))
-    for i in range(n):
-        dd[i][0] = _dd(ys[i], ys[i + 1], xs[0], xs[1])
-    for j in range(1, n):
-        for i in range(n - j):
-            dd[i][j] = _dd(dd[i][j - 1], dd[i + 1][j - 1], xs[i], xs[i + j + 1])
-    return dd
+def to_float(matrix):
+    for row in matrix:
+        for i in range(len(row)):
+            row[i] = float(row[i])
 
-def lower_bound(lst, key):
-    left = -1
-    right = len(lst)
-    while left + 1 < right:
-        middle = (left + right) // 2
-        if lst[middle] >= key:
-            right = middle
+def swap_columns(matrix):
+    new_matrix = copy.deepcopy(matrix)
+    for row in new_matrix:
+        row[0], row[1] = row[1], row[0]
+    return new_matrix
+
+def multiply_rows(matrix):
+    new_matrix = []
+    for row in matrix:
+        new_matrix.append(row)
+        new_matrix.append(row)
+
+    return new_matrix
+
+def input_information():
+    # filename = input("Введите имя файла с данными: ")
+    filename = "table"
+    try:
+        with open(filename) as file:
+            matrix = [row.split() for row in file.readlines()]
+        arg = float(input("Введите значение аргумента: "))
+    except:
+        print("Ошибка. Данные представлены в некорректном формате")
+        exit()
+    to_float(matrix)
+    return matrix, arg
+
+def print_results(newton, hermite, newton_root, hermite_root):
+    print("Ньютон: {:.6f}".format(newton))
+    print("Эрмит: {:.6f}".format(hermite))
+    print("Корень(Ньютон): {:.6f}".format(newton_root))
+    print("Корень(Эрмит): {:.6f}".format(hermite_root))
+
+def get_range(matrix, degree, arg):
+    for right in range(len(matrix)):
+        if matrix[right][0] > arg:
+            break
+    left = right
+    while right - left < degree:
+        if left > 0:
+            left -= 1
+        if right - left > degree or left == 0 and right == len(matrix) - 1:
+            break
+        if right < len(matrix) - 1:
+            right += 1
+    if matrix[left][0] == matrix[left - 1][0]:
+        left -= 1
+        right -= 1
+    return left, right - 1
+
+def get_diff_matrix(matrix, rng):
+    diff_matrix = [[], []]
+    der_matrix = []
+    for i in range(rng[0], rng[1] + 1):
+        diff_matrix[0].append(matrix[i][0])
+        diff_matrix[1].append(matrix[i][1])
+        der_matrix.append(matrix[i][2])
+    for i in range(1, rng[1] - rng[0] + 1):
+        row = []
+        for j in range(rng[1] - rng[0] - i + 1):
+            if diff_matrix[0][j] - diff_matrix[0][j + i] == 0:
+                row.append(der_matrix[j])
+                continue
+            row.append(
+                (diff_matrix[i][j] - diff_matrix[i][j + 1])
+                / (diff_matrix[0][j] - diff_matrix[0][j + i])
+            )
+        diff_matrix.append(row)
+    return diff_matrix
+
+def print_diff_matrix(matrix):
+    i = 0
+    while i < len(matrix[0]):
+        for row in matrix:
+            if i < len(row):
+                print(f'{row[i]:2.5f}   ', end='')
+        i += 1
+        print()
+
+
+def polynom(matrix, degree, arg):
+    rng = get_range(matrix, degree, arg)
+    diffs_table = get_diff_matrix(matrix, rng)
+    # print("Range:", rng)
+    print_diff_matrix(diffs_table)
+    print('-------------------------------------------------------')
+    mul = 1
+    val = diffs_table[1][0]
+    for i in range(2, len(diffs_table)):
+        mul *= (arg - diffs_table[0][i - 2])
+        val += diffs_table[i][0] * mul
+    return val
+
+
+def calc_root(diffs_table, arg):
+    val = diffs_table[1][0]
+    mul = 1
+    for i in range(2, len(diffs_table)):
+        mul *= (arg - diffs_table[0][i - 2])
+        val += diffs_table[i][0] * mul
+    return val
+
+def get_root(matrix, degree, arg):
+    rng = get_range(matrix, degree, arg)
+    diffs_table = get_diff_matrix(matrix, rng)
+    # print("Range:", rng)
+    print_diff_matrix(diffs_table)
+    print('-------------------------------------------------------')
+    mul = 1
+    val = diffs_table[1][0]
+    
+    lo = diffs_table[0][0]
+    hi = diffs_table[0][len(diffs_table[0]) - 1]
+    while lo < hi:
+        mid = (lo + hi)//2
+        midval = calc_root(diffs_table, mid)
+        if abs(midval) < 1e-7:
+            return mid
+        elif midval < 0:
+            lo = mid + 1
+        elif midval > 0:
+            hi = mid
+    return -1
+
+def change_diff(matrix):
+    new_matrix = []
+    for row in matrix:
+        if row[2] != 0:
+            row[2] = 1 / row[2]
         else:
-            left = middle
-    return right
+            row[2] = inf
+        new_matrix.append(row)
+    return new_matrix
 
-def nodes(xs, ys, n, x):
-    s = [[xs[i], ys[i]] for i in range(len(xs))]
-    s = np.array(sorted(s, key=lambda x: x[0]))
-    xs, ys = s[:,0], s[:,1]
-    xm = lower_bound(xs, x)
-    rh = n // 2
-    lh = n - rh
-    
-    start = xm - lh
-    stop = xm + rh
-
-    if start < 0:
-        stop -= start
-        start = 0
-    elif stop >= len(xs):
-        start -= stop - len(xs) + 1
-        stop = len(xs) - 1
-    
-    return xs[start:stop+1], ys[start:stop+1]
-
-def newton_polynomial(xs, ys, n, x):
-    xs, ys = nodes(xs, ys, n, x)
-    dd = divided_difference(xs, ys, n)
-    xc, y = 1, ys[0]
-    for i in range(n):
-        xc *= x - xs[i]
-        y += xc * dd[0][i]
-    return y
-
-def print_title(title):
-    print('─' * 4, title, '─' * (80 - 4 - 1 - len(title) - 1))
-
-z = 0
-def assign_z_to(value):
-    global z
-    z = value
-
-def spline_interpolation(xs, ys, x):
-    j = lower_bound(xs, x)
-
-    n = len(xs)
-    h = np.zeros((n))
-    A = np.zeros((n))
-    B = np.zeros((n))
-    D = np.zeros((n))
-    F = np.zeros((n))
-    a = np.zeros((n))
-    b = np.zeros((n))
-    c = np.zeros((n + 1))
-    d = np.zeros((n))
-    xi = np.zeros((n + 1))
-    eta = np.zeros((n + 1))
-
-    for i in range(1, n):
-        h[i] = xs[i] - xs[i-1]
-
-    for i in range(2, n):
-        A[i] = h[i-1]
-        B[i] = -2 * (h[i-1] + h[i])
-        D[i] = h[i]
-        F[i] = -3 * ((ys[i] - ys[i-1]) / h[i] - (ys[i-1] - ys[i-2]) / h[i-1])
-
-    for i in range(2, n):
-        xi[i+1] = D[i] / (B[i] - A[i] * xi[i])
-        eta[i+1] = (A[i] * eta[i] + F[i]) / (B[i] - A[i] * xi[i])
-
-    for i in range(n - 2, -1, -1):
-        c[i] = xi[i] * c[i+1] + eta[i+1]
-
-    for i in range(1, n):
-        a[i] = ys[i-1]
-        b[i] = (ys[i] - ys[i-1]) / h[i] - h[i] / 3 * (c[i+1] + 2 * c[i])
-        d[i] = (c[i+1] - c[i]) / (3 * h[i])
-
-    return a[j] \
-         + b[j] * (x - xs[j-1]) \
-         + c[j] * ((x - xs[j-1]) ** 2) \
-         + d[j] * ((x - xs[j-1]) ** 3)
+def inf_filter(matrix):
+    new_matrix = []
+    for row in matrix:
+        if row[2] != inf:
+            new_matrix.append(row)
+    return new_matrix
 
 def main():
-    print_title('List of tables')
-    list_tables()
+    inform = input_information()
+    table, arg_val = inform[0], inform[1]
+    table = sorted(table)
+    ext_table = multiply_rows(table)
+    newton_table = table
+    # new_table = inf_filter(multiply_rows(change_diff(swap_columns(sorted(table)))))
+    new_table = multiply_rows(sorted(table))
+    for degree in range(1, 6):
+        print("n = {}".format(degree))
+        newton = polynom(table, degree, arg_val)
+        hermite = polynom(ext_table, degree, arg_val)
+        newton_root = get_root(newton_table, degree, 0)
+        hermite_root = get_root(new_table, degree, 0)
+        print_results(newton, hermite, newton_root, hermite_root)
 
-    print_title('Menu:')
-    filename = input('Input filename: ')
-    title, xs, ys = load_table(filename)
-
-    print_title('Table:')
-    print_table(title, xs, ys)
-
-    print_title('Menu:')
-    x = float(input('Input x: '))
-
-    y = spline_interpolation(xs, ys, x)
-    eval('assign_z_to({})'.format(title))
-
-    ae = fabs(z - y)
-    re = ae / fabs(z)
-
-    print('Interpolated value: {:.3f}'.format(y))
-    print('Real value        : {:.3f}'.format(z))
-    print('Absolute error    : {:.3f}'.format(ae))
-    print('Relative error    : {:.3f}'.format(re))
-
-# def main():
-#     print_title('List of tables:')
-#     list_tables()
-    
-#     print_title('Menu:')
-#     filename = input('Input filename: ')
-#     title, xs, ys = load_table(filename)
-
-#     print_title('Table:')
-#     print_table(title, xs, ys)
-
-#     print_title('Menu:')
-#     x = float(input('Input x: '))
-#     n = int(input('Input n: '))
-
-#     y = newton_polynomial(xs, ys, n, x)
-#     if 'x' in title:
-#         eval('assign_z_to({})'.format(title))
-#     else:
-#         global z
-#         z = float(title)
-
-#     ae = fabs(z - y)
-#     re = ae / fabs(z)
-
-#     print('Interpolated value: {:.3f}'.format(y))
-#     print('Real value        : {:.3f}'.format(z))
-#     print('Absolute error    : {:.3f}'.format(ae))
-#     print('Relative error    : {:.3f}'.format(re))
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
